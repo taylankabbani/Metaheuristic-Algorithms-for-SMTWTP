b5919348444d5f31f5fd905c9d92c87e83eaab02
taylankabbani <taylankabbani96@gmail.com>
2020-12-07 22:09:12 +0300

    SA 1.3

--- SA_Algorithm.py
+++ SA_Algorithm.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random as rd
+import math
 
 class SA():
     def __init__(self, Path, initial_temp, epoch, cooling_rate):
@@ -44,6 +45,7 @@ class SA():
         if show == True:
             print("\n","#"*8, "The Objective function value for {} solution schedule is: {}".format(solution ,objfun_value),"#"*8)
         return objfun_value
+
     def SwapMove(self, li, show = False):
         '''Takes a list (solution)
         returns a new neighbor solution with randomly swapped elements
@@ -63,16 +65,72 @@ class SA():
         Input : Initial temp(T_0), Epoch length(epo), Stopping Temp(T_f)
         Output: Best solution found and the Objfun Value of the corresponding solution
         '''
-        initial_solution = self.get_InitialSolution()  # Randomly assign the initial solution
+    
         # SA Parameters:
         T_0 = self.initial_temp
-        epo = self.epoch
+        maxepo = self.epoch
         cooling_fun = lambda x: x * self.cooling_rate
         T_f = 0.001
 
+        best_solution = []
+        best_objvalue = float('inf')
+        current_solution = self.get_InitialSolution()  # Randomly assign the initial solution at the first iteration.
+        current_objvalue = self.Objfun(current_solution)
+
+        T = T_0  # Current temp
+        current_iter = 1
+        print("Number of jobs to be scheduled: {}".format(len(current_solution)))
+        print("Initial Solution: {}    Initial Objfun value: {}\n\n".format(current_solution, current_objvalue))
+
+        while T >= T_f :
+            epoch = int(0)
+            while epoch < maxepo:
+
+                print("### iter: {}/ epoch: {} ### current_temp: {}, current_objfun: {}, best_Objfun: {}".format(current_iter, epoch, T,current_objvalue,best_objvalue))
+
+                #generate neighbor by randomly swap move
+                candidate_solution = self.SwapMove(current_solution) 
+                candidate_objvalue = self.Objfun(candidate_solution)
+
+                # Improving solution found
+                if candidate_objvalue <= current_objvalue:  
+                    current_solution, current_objvalue = candidate_solution ,candidate_objvalue
+                   # Update best solution 
+                    if current_objvalue < best_objvalue:
+                        best_solution, best_objvalue = current_solution, current_objvalue
+
+                    print(" "*5, "Candidate Solution: Objfun= {},  Sequence= {}   =>  Improved Solution  => Accepted\n".format(candidate_objvalue, candidate_solution))   
+                
+                #non_imporving solution
+                else:
+                    degradation = candidate_objvalue - current_objvalue
+                    metropolis = math.exp(-(degradation/T))
+                    x = rd.random()
+                    # Accepting non-improvment solution based on metropolis criterion
+                    if x <= metropolis:
+                        current_solution, current_objvalue = candidate_solution ,candidate_objvalue
+                        print(" "*5, "Candidate Solution: Objfun= {},  Sequence= {}   =>  Non_improved Solution => Metropolis => Accepted\n".format(candidate_objvalue, candidate_solution))   
+                    else:
+                        print(" "*5, "Candidate Solution: Objfun= {},  Sequence= {}   =>  Non_improved Solution => Metropolis  => Not Accepted\n".format(candidate_objvalue, candidate_solution)) 
+                #Update param
+                epoch += 1
+            # Update Temp & iter
+            T = cooling_fun(T)
+            current_iter += 1
+        print('#'*30, '\nNumber of iteration performed: {}\nFinal Temperature: {}\nBest Objvalue: {}\nBest Solution: {}'.format(current_iter, T, best_objvalue, best_solution))
+                    
 
 
 
-instances_10 = SA(Path="Data_instances/Instance_10.xlsx")
-initial = instances_10.get_InitialSolution()
-objvalue = instances_10.Objfun(initial,True)
+        
+
+
+
+
+
+
+
+
+
+instances_10 = SA(Path="Data_instances/Instance_10.xlsx", initial_temp = 1000, epoch = 3, cooling_rate = 0.99)
+instances_10.SimuAnn()
