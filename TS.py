6189fc23787024df8a617045a4c0ecb46adb5389
taylankabbani <taylankabbani96@gmail.com>
2020-12-15 23:57:19 +0300

    TS 1.2

--- TS.py
+++ TS.py
@@ -4,9 +4,10 @@ from itertools import combinations
 import math
 
 class TS():
-    def __init__(self, Path, seed):
+    def __init__(self, Path, seed, tabu_tenure):
         self.Path = Path
         self.seed = seed
+        self.tabu_tenure = tabu_tenure
         self.instance_dict = self.input_data()
         self.Initial_solution = self.get_InitialSolution()
 
@@ -20,12 +21,11 @@ class TS():
 
     def get_tabuestructure(self):
         '''Takes a dict (input data)
-        Returns a dict of tabu attributes(pair of jobs that are swapped) as keys and [tabu_time, SwapValue (for
-        candidate solutions)]
+        Returns a dict of tabu attributes(pair of jobs that are swapped) as keys and [tabu_time, MoveValue]
         '''
         dict = {}
         for swap in combinations(self.instance_dict.keys(), 2):
-            dict[swap] = {'tabu_time': 0, 'SwapValue': 0}
+            dict[swap] = {'tabu_time': 0, 'MoveValue': 0}
         return dict
 
     def get_InitialSolution(self, show=False):
@@ -70,17 +70,59 @@ class TS():
         return solution
 
     def TSearch(self):
-        '''The implementation Tabu search algorithm with short-term memory and pair swap as Tabu attribute.
-        Input :
-        Output: Best solution found and the Objfun Value of the corresponding solution
+        '''The implementation Tabu search algorithm with short-term memory and pair_swap as Tabu attribute.
         '''
+        # Parameters:
+        tenure =self.tabu_tenure
         tabu_structure = self.get_tabuestructure()  # Initialize the data structures
+        best_solution = self.Initial_solution
+        best_objvalue = self.Objfun(best_solution)
         current_solution = self.Initial_solution
         current_objvalue = self.Objfun(current_solution)
-        # print("Initial Solution: {}, objvalue: {}".format(self.Initial_solution, self))
 
-        # Searching the whole neighborhood of the current solution:
-        for neighbor in
+        print("#"*30, "Short-term memory TS with Tabu Tenure: {}\nInitial Solution: {}, Initial Objvalue: {}".format(
+            tenure, current_solution, current_objvalue), "#"*30, sep='\n\n')
+        iter = 1
+        Terminate = 0
+        while Terminate < 100:
+            print('\n\n### iter {}###  Current_Objvalue: {}, Best_Objvalue: {}'.format(iter, current_objvalue,
+                                                                                    best_objvalue))
+            # Searching the whole neighborhood of the current solution:
+            for move in tabu_structure:
+                candidate_solution = self.SwapMove(current_solution, move[0], move[1])
+                candidate_objvalue = self.Objfun(candidate_solution)
+                tabu_structure[move]['MoveValue'] = candidate_objvalue
+
+            # Admissible move
+            while True:
+                # select the move with the lowest ObjValue in the neighborhood (minimization)
+                best_move = min(tabu_structure, key =lambda x: tabu_structure[x]['MoveValue'])
+                MoveValue = tabu_structure[best_move]["MoveValue"]
+                tabu_time = tabu_structure[best_move]['tabu_time']
+                # Aspiration criteria and not tabu
+                if tabu_time < iter or MoveValue < best_objvalue:
+                    # make the move
+                    current_solution = self.SwapMove(current_solution, best_move[0], best_move[1])
+                    current_objvalue = self.Objfun(current_solution)
+                    if current_objvalue < best_objvalue:
+                        best_solution = current_solution
+                        best_objvalue = current_objvalue
+                        print("   best_move: {}, Objvalue: {} => Most imporving => Admissible".format(best_move,
+                                                                                                      current_objvalue))
+                    else:
+                        print("   best_move: {}, Objvalue: {} => Least non-improving => Admissible".format(best_move,
+                                                                                                      current_objvalue))
+                        Terminate += 1
+                    # update tabu_time for the move
+                    tabu_structure[best_move]['tabu_time'] += tenure
+                    iter += 1
+                    break
+                else:
+                    tabu_structure[best_move]["MoveValue"] = float('inf')
+                    print("   best_move: {}, Objvalue: {} => Tabu => Inadmissible".format(best_move, current_objvalue))
+                    continue
+        print('#'*50)
+        print(best_solution,best_objvalue)
 
 
 
@@ -92,4 +134,11 @@ class TS():
 
 
 
-test = TS(Path="Data_instances/Instance_10.xlsx",seed = 2012)
+
+
+
+
+
+
+test = TS(Path="Data_instances/Instance_30.xlsx",seed = 2019, tabu_tenure=3)
+test.TSearch()
